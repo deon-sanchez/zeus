b7c7fd075ec8b3b921272d03b970768b00f48c1b
Deon Sanchez <deon.sanchez@datastax.com>
2024-02-29 15:25:06 -0700

    Add new screens and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+
+import HomeScreen from './screens/HomeScreen';
+import PersonalScreen from './screens/PersonalScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const App = (): React.JSX.Element => {
+  const Tab = createMaterialTopTabNavigator();
+
   return (
     <NavigationContainer>
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <View>
-            <Text>Hello world</Text>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <Tab.Navigator tabBarPosition="bottom">
+        <Tab.Screen name="Personal" component={PersonalScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
---
+++ src/screens/PersonalScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const PersonalScreen = (): React.JSX.Element => {
+  return (
+    <View>
+      <Text>PersonalScreen</Text>
+    </View>
+  );
+};
+
+export default PersonalScreen;
